492e2b253bfbe1cf87f44b1b448c76f15a32eebb
Davi Nakajima An <davi.nakajima.an@gmail.com>
2023-12-04 22:53:24 -0800

    Make this pip installable

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='kinetics_api',
+    version='0.1.0',    
+    description='An open-source package for accessing chemical kinetics databases',
+    url='https://github.com/davinan/kinetics_db_interface/',
+    author='Davi Nakajima An',
+    author_email='dnan@uw.edu',
+    license='MIT License',
+    packages=['kinetics_interface'],
+    install_requires=['tellurium',
+                      'numpy',                     
+                      'pandas',
+                      'requests',
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
