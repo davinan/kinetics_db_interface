0b09fb303d76032c0f8e38e28b0b583f927d98b5
Davi Nakajima An <dnan@uw.edu>
2023-12-15 18:05:59 -0800

    Documentation

--- kinetics_interface/query.py
+++ kinetics_interface/query.py
@@ -8,11 +8,34 @@ PARAM_QUERY_URL = 'http://sabiork.h-its.org/entry/exportToExcelCustomizable'
 
 
 class SabioRKQuery():
+    """
+    A class for querying the SABIO-RK database for reaction kinetics information.
+
+    This class allows for the construction of a query based on specific biochemical parameters
+    such as pathway, organism, enzyme commission (EC) number, substrate, and product. The query
+    can be used to retrieve reaction data, including kinetic parameters, from the SABIO-RK database.
+
+    Attributes:
+        query (dict): A dictionary representing the query parameters to be sent to the SABIO-RK API.
+
+    Methods:
+        get_reactions: Retrieve reactions from SABIO-RK based on the query parameters.
+    """
     
     def __init__(self, pathway=None, organism=None, ecnumber=None, substrate=None, product=None):
-        entryIDs = []
-        # ask SABIO-RK for all EntryIDs matching a query
+        """
+        Initialize the SabioRKQuery object with optional search parameters.
 
+        Args:
+            pathway (str, optional): The biochemical pathway to filter reactions.
+            organism (str, optional): The organism to filter reactions.
+            ecnumber (str, optional): The enzyme commission (EC) number to filter reactions.
+            substrate (str, optional): The substrate to filter reactions.
+            product (str, optional): The product to filter reactions.
+
+        The constructor builds a query string that can be used to search the SABIO-RK database
+        for reactions that match the given criteria.
+        """
         query_dict = {}
         if pathway is not None:
             query_dict['Pathway'] = pathway
@@ -29,9 +52,63 @@ class SabioRKQuery():
 
         query_string = ' AND '.join(['%s:%s' % (k,v) for k,v in query_dict.items()])
         self.query = {'format':'txt', 'q':query_string}
+    
+    def update_query(self, pathway=None, organism=None, ecnumber=None, substrate=None, product=None):
+        """
+        Update the existing query parameters for the SABIO-RK database search.
 
+        This method allows modifying the query parameters like pathway, organism, EC number, 
+        substrate, and product after the SabioRKQuery object has been initialized.
+
+        Args:
+            pathway (str, optional): The biochemical pathway to filter reactions.
+            organism (str, optional): The organism to filter reactions.
+            ecnumber (str, optional): The enzyme commission (EC) number to filter reactions.
+            substrate (str, optional): The substrate to filter reactions.
+            product (str, optional): The product to filter reactions.
+
+        The method updates the query dictionary and then reconstructs the query string based on 
+        the updated parameters. This new query string is used for subsequent searches.
+        """
+
+        if pathway is not None:
+            self.query['Pathway'] = pathway
+        if organism is not None:
+            self.query['Organism'] = organism
+        if ecnumber is not None:
+            self.query['ECNumber'] = ecnumber
+        if substrate is not None:
+            self.query['Substrate'] = substrate
+        if product is not None:
+            self.query['Product'] = product
+
+        # Reconstruct the query string based on the updated query dictionary
+        query_string = ' AND '.join(['%s:%s' % (k,v) for k,v in self.query.items() if k != 'format' and k != 'q'])
+        self.query['q'] = query_string
 
     def get_reactions(self, max_search=500):
+        """
+        Retrieve reaction data from the SABIO-RK database based on the query parameters.
+
+        This method performs a GET request to obtain entry IDs based on the query, and then
+        a POST request to retrieve detailed reaction data for those entries. It filters and
+        processes this data to return valid enzyme reactions.
+
+        Args:
+            max_search (int, optional): The maximum number of search results to process. Defaults to 500.
+
+        Returns:
+            tuple: A tuple containing two elements:
+                - DataFrame: A pandas DataFrame containing detailed reaction data.
+                - list: A list of EnzymeReaction objects that have valid Km and Vmax parameters.
+
+        Raises:
+            HTTPError: If the request to the SABIO-RK database fails.
+
+        The method first performs a GET request to obtain a list of entry IDs matching the query.
+        It then constructs a new query to get detailed parameters for these entries and makes a POST request.
+        Finally, it processes the data to filter out valid enzyme reactions based on kinetic parameters.
+        """
         # make GET request
         request = requests.get(ENTRYID_QUERY_URL, params=self.query)
         request.raise_for_status() # raise if 404 error
@@ -51,8 +128,6 @@ class SabioRKQuery():
             'UniprotID',
             'ECNumber', 
             'PubMedID',
-            # 'TemperatureRange',
-            # 'pHValueRange',
             'Parameter',
             'ReactomeReactionID', 
             'HasKineticData', 
--- kinetics_interface/reaction.py
+++ kinetics_interface/reaction.py
@@ -2,9 +2,40 @@ from typing import List, Dict
 import tellurium as te
 
 class EnzymeReaction:
+    """
+    A class representing an enzyme reaction, defined by kinetic parameters and initial conditions.
+
+    Attributes:
+        df (DataFrame): A pandas DataFrame containing the reaction parameters.
+        S_start (float): The starting concentration of the substrate.
+        E_start (float): The starting concentration of the enzyme.
+        Km (float): The Michaelis constant of the reaction.
+        Vmax (float): The maximum velocity of the reaction.
+        k1 (float): The rate constant for the formation of the enzyme-substrate complex.
+        name (str): A unique identifier for the reaction.
+        _is_valid (bool): Indicates whether the reaction parameters are valid and complete.
+
+    Methods:
+        change_value: Update reaction parameters.
+        get_reaction_details: Return the reaction parameters DataFrame.
+        is_valid: Check if the reaction parameters are valid and complete.
+        get_model: Generate a textual representation of the reaction model.
+        plot_tellurium: Simulate and plot the reaction using Tellurium.
+    """
 
     def __init__(self, reaction_df):
+        """
+        Initialize the EnzymeReaction object with a DataFrame containing reaction parameters.
 
+        Args:
+            reaction_df (DataFrame): A DataFrame containing parameters like Km, Vmax,
+                                     and concentrations with columns 'parameter.type', 
+                                     'parameter.startValue', and 'parameter.endValue'.
+
+        The initialization process extracts necessary parameters (Km, Vmax, starting concentrations)
+        from the DataFrame and checks for the presence of essential data to determine if the reaction
+        is valid.
+        """
         self.df = reaction_df
         Km_rows = self.df[self.df['parameter.type'] == 'Km']
         Vmax_rows = self.df[self.df['parameter.type'] == 'Vmax']
@@ -30,6 +61,19 @@ class EnzymeReaction:
         self.name = f"SabioRKID{self.df.iloc[0].EntryID}"
 
     def change_value(self, Km=None, Vmax=None, S_start=None, E_start=None, k1=None):
+        """
+        Update reaction parameters like Km, Vmax, starting concentrations, and rate constant.
+
+        Args:
+            Km (float, optional): New Michaelis constant.
+            Vmax (float, optional): New maximum velocity.
+            S_start (float, optional): New starting substrate concentration.
+            E_start (float, optional): New starting enzyme concentration.
+            k1 (float, optional): New rate constant for the formation of the enzyme-substrate complex.
+
+        Returns:
+            DataFrame: The updated DataFrame containing the reaction parameters.
+        """
         if Km is not None: self.Km = Km
         if k1 is not None: self.k1 = k1
         if Vmax is not None: self.Vmax = Vmax
@@ -38,12 +82,30 @@ class EnzymeReaction:
         return self.get_reaction_details()
 
     def get_reaction_details(self):
+        """
+        Return the DataFrame containing reaction parameters.
+
+        Returns:
+            DataFrame: The DataFrame representing the reaction parameters.
+        """
         return self.df
 
     def is_valid(self):
+        """
+        Check if the reaction parameters are valid and complete
+
+        Returns:
+            bool: True if the reaction parameters are valid and complete, False otherwise.
+        """
         return self._is_valid
 
     def get_model(self):
+        """
+        Generate a Antimony representation of the reaction model.
+
+        Returns:
+            str: A string representation of the reaction model using kinetic parameters.
+        """
         model = f"model {self.name}\n"
         model += f"\n\tspecies E, S, ES, P;\n"
         model += f"\n\tVmax = {self.Vmax};"
@@ -52,18 +114,24 @@ class EnzymeReaction:
         model += f"\n\tS = {self.S_start};\n"
         model += f"\n\tJ0: E + S -> ES; k1*E*S"
         model += f"\n\tJ1: ES -> E + S; (1/k1)*ES"
-        model += f"\n\tJ2: ES -> E + P; Vmax * S / (Km + S)\n"
+        model += f"\n\tJ2: ES -> E + P; Vmax * ES / (Km + S)\n"
         model += f"\n\tk1 = {self.k1};"
         model += f"\nend"
         return model
 
     def plot_tellurium(self, range=(0, 100), num_timesteps=1000):
+        """
+        Simulate and plot the reaction using Tellurium.
+
+        Args:
+            range (tuple, optional): The time range for the simulation (start, end).
+            num_timesteps (int, optional): The number of timesteps in the simulation.
+
+        Returns:
+            numpy.ndarray: The simulation data as an array.
+        """
         te.setDefaultPlottingEngine('matplotlib')
-        # load models
         r = te.loada(self.get_model())
         data = r.simulate(range[0], range[1], num_timesteps)
-        # plot the simulation
-        # plt.title(R.get_reaction_string())
-        # r.plot()
         r.plot(xtitle='Time', ytitle='Concentration', title=self.name)
         return data
